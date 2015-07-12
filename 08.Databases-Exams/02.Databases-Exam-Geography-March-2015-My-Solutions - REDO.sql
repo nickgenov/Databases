9ccadbda5a901a98ec6aa4c6204b74a8508d6e3e
nickgenov <nickolay.r.genov@gmail.com>
2015-07-12 12:20:35 +0300

    Exam practice

--- 08.Databases-Exams/02.Databases-Exam-Geography-March-2015-My-Solutions - REDO.sql
+++ 08.Databases-Exams/02.Databases-Exam-Geography-March-2015-My-Solutions - REDO.sql
@@ -655,3 +655,124 @@ GO
 
 
 SELECT DBO.fn_MountainsPeaksJSON()
+
+/*
+Problem 18.	Design a Database Schema in MySQL and Write a Query
+1.	Design a MySQL database "trainings" to hold training centers, courses and a course timetable. 
+Courses hold name and optional description. Training centers hold name, optional description and optional URL. 
+The course timetable holds a set of timetable items, each consisting of course, training center and starting date. 
+All tables should have auto-increment primary key – id. All text fields should accept Unicode characters.
+*/
+
+/*
+DROP DATABASE IF EXISTS `trainings`;
+
+CREATE DATABASE `trainings`
+CHARACTER SET utf8 COLLATE utf8_unicode_ci;
+
+USE `trainings`;
+
+DROP TABLE IF EXISTS `training_centers`;
+
+CREATE TABLE `training_centers` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(45) NOT NULL,
+  `description` text,
+  `url` varchar(2083),
+  PRIMARY KEY (`id`)
+);
+
+DROP TABLE IF EXISTS `courses`;
+
+CREATE TABLE `courses` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(100) NOT NULL,
+  `description` text,
+  PRIMARY KEY (`id`)
+);
+
+DROP TABLE IF EXISTS `courses_timetable`;
+
+CREATE TABLE `timetable` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `course_id` int(11) NOT NULL,
+  `training_center_id` int(11) NOT NULL,
+  `start_date` date NOT NULL,
+  PRIMARY KEY (`id`),
+  CONSTRAINT `fk_courses_timetable_courses`
+    FOREIGN KEY (`course_id`) 
+    REFERENCES `courses` (`id`),
+  CONSTRAINT `fk_courses_timetable_training_centers` 
+    FOREIGN KEY (`training_center_id`) 
+    REFERENCES `training_centers` (`id`)
+);
+*/
+
+/*
+2.	Execute the following SQL script to load data in your tables. It should pass without any errors:
+*/
+
+/*
+INSERT INTO `training_centers` VALUES
+(1, 'Sofia Learning', NULL, 'http://sofialearning.org'),
+(2, 'Varna Innovations & Learning', 'Innovative training center, located in Varna. Provides trainings in software development and foreign languages', 'http://vil.edu'),
+(3, 'Plovdiv Trainings & Inspiration', NULL, NULL),
+(4, 'Sofia West Adult Trainings', 'The best training center in Lyulin', 'https://sofiawest.bg'),
+(5, 'Software Trainings Ltd.', NULL, 'http://softtrain.eu'),
+(6, 'Polyglot Language School', 'English, French, Spanish and Russian language courses', NULL),
+(7, 'Modern Dances Academy', 'Learn how to dance!', 'http://danceacademy.bg');
+
+INSERT INTO `courses` VALUES
+(101, 'Java Basics', 'Learn more at https://softuni.bg/courses/java-basics/'),
+(102, 'English for beginners', '3-month English course'),
+(103, 'Salsa: First Steps', NULL),
+(104, 'Avancée Français', 'French language: Level III'),
+(105, 'HTML & CSS', NULL),
+(106, 'Databases', 'Introductionary course in databases, SQL, MySQL, SQL Server and MongoDB'),
+(107, 'C# Programming', 'Intro C# corse for beginners'),
+(108, 'Tango dances', NULL),
+(109, 'Spanish, Level II', 'Aprender Español');
+
+INSERT INTO `timetable`(course_id, training_center_id, start_date) VALUES
+(101, 1, '2015-01-31'), (101, 5, '2015-02-28'),
+(102, 6, '2015-01-21'), (102, 4, '2015-01-07'), (102, 2, '2015-02-14'), (102, 1, '2015-03-05'), (102, 3, '2015-03-01'),
+(103, 7, '2015-02-25'), (103, 3, '2015-02-19'),
+(104, 5, '2015-01-07'), (104, 1, '2015-03-30'), (104, 3, '2015-04-01'),
+(105, 5, '2015-01-25'), (105, 4, '2015-03-23'), (105, 3, '2015-04-17'), (105, 2, '2015-03-19'),
+(106, 5, '2015-02-26'),
+(107, 2, '2015-02-20'), (107, 1, '2015-01-20'), (107, 3, '2015-03-01'), 
+(109, 6, '2015-01-13');
+
+UPDATE `timetable` t
+  JOIN `courses` c ON t.course_id = c.id
+SET t.start_date = DATE_SUB(t.start_date, INTERVAL 7 DAY)
+WHERE c.name REGEXP '^[a-j]{1,5}.*s$';
+*/
+
+/*
+3.	Write a SQL query to list all entries from the timetable ordered by start date and then by id. 
+Display the training center, start date, course name and more info about the course (course details). 
+Name the columns exactly like in the table below:
+*/
+
+/*
+SELECT 
+  tc.name AS `traning center`,
+  t.start_date AS `start date`,
+  c.name AS `course name`,
+  c.description AS `more info`
+FROM `timetable` t
+  JOIN `courses` c ON t.course_id = c.id
+  JOIN `training_centers` tc ON t.training_center_id = tc.id
+ORDER BY t.start_date, t.id
+
+select 
+	tc.name as 'training center',
+    t.start_date as 'start date',
+    c.name as 'course name',
+    c.description as 'more info'
+from timetable t 
+join training_centers tc on tc.id = t.training_center_id
+join courses c on c.id = t.course_id
+order by t.start_date asc, t.id asc;
+*/
---
+++ 08.Databases-Exams/02.Task-18-Trainings-MySQL-Solution.sql
@@ -0,0 +1,90 @@
+CREATE DATABASE  IF NOT EXISTS `trainings` /*!40100 DEFAULT CHARACTER SET utf8 COLLATE utf8_unicode_ci */;
+USE `trainings`;
+-- MySQL dump 10.13  Distrib 5.6.23, for Win64 (x86_64)
+--
+-- Host: localhost    Database: trainings
+-- ------------------------------------------------------
+-- Server version	5.6.25-log
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `courses`
+--
+
+DROP TABLE IF EXISTS `courses`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `courses` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(50) CHARACTER SET utf8 NOT NULL,
+  `description` varchar(200) CHARACTER SET utf8 DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=110 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `timetable`
+--
+
+DROP TABLE IF EXISTS `timetable`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `timetable` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `course_id` int(11) NOT NULL,
+  `training_center_id` int(11) NOT NULL,
+  `start_date` datetime NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `fk_timetable_courses_idx` (`course_id`),
+  KEY `fk_timetable_training_centers_idx` (`training_center_id`),
+  CONSTRAINT `fk_timetable_courses` FOREIGN KEY (`course_id`) REFERENCES `courses` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION,
+  CONSTRAINT `fk_timetable_training_centers` FOREIGN KEY (`training_center_id`) REFERENCES `training_centers` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION
+) ENGINE=InnoDB AUTO_INCREMENT=22 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `training_centers`
+--
+
+DROP TABLE IF EXISTS `training_centers`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `training_centers` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(50) CHARACTER SET utf8 NOT NULL,
+  `description` varchar(200) CHARACTER SET utf8 DEFAULT NULL,
+  `url` varchar(200) CHARACTER SET utf8 DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=8 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2015-07-12 12:03:13
+
+select 
+	tc.name as 'training center',
+    t.start_date as 'start date',
+    c.name as 'course name',
+    c.description as 'more info'
+from timetable t 
+join training_centers tc on tc.id = t.training_center_id
+join courses c on c.id = t.course_id
+order by t.start_date asc, t.id asc;
